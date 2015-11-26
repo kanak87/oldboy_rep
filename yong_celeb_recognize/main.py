b547f03814898fc56d007c71bfe059cf584ba19d
Daehee Han <handol@gmail.com>
2015-11-26 17:15:50 +0900

    x-windows-949 encoding 오류 발생 - 해결 시도중.

--- yong_celeb_recognize/main.py
+++ yong_celeb_recognize/main.py
@@ -2,6 +2,11 @@ from nn import *
 from nn2 import *
 from image import *
 
+# sys.setdefaultencoding() does not exist, here!
+import sys
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
 # left_eye_center_x,left_eye_center_y,right_eye_center_x,right_eye_center_y,left_eye_inner_corner_x,left_eye_inner_corner_y,left_eye_outer_corner_x,left_eye_outer_corner_y,right_eye_inner_corner_x,right_eye_inner_corner_y,right_eye_outer_corner_x,right_eye_outer_corner_y,left_eyebrow_inner_end_x,left_eyebrow_inner_end_y,left_eyebrow_outer_end_x,left_eyebrow_outer_end_y,right_eyebrow_inner_end_x,right_eyebrow_inner_end_y,right_eyebrow_outer_end_x,right_eyebrow_outer_end_y,nose_tip_x,nose_tip_y,mouth_left_corner_x,mouth_left_corner_y,mouth_right_corner_x,mouth_right_corner_y,mouth_center_top_lip_x,mouth_center_top_lip_y,mouth_center_bottom_lip_x,mouth_center_bottom_lip_y,Image
 '''
 net = loadNet('net1.pickle')
